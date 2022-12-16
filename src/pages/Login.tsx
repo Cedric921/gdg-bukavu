74fb6ebf12a813bef950b6d6df778f57b0cafc5d
ItVal <valentinhonvj1@gmail.com>
2022-12-16 17:54:51 +0100

    google login implementation

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, {useEffect}from 'react';
 import {GoogleLogin} from 'react-google-login';
 import { gapi } from 'gapi-script';
 
 const Login = () => {
-	const responseGoogle = (response: any) => {
-		console.log(response);
-	};
+	const clientId = import.meta.env.VITE_GOOGLE_KEY;
 
+	useEffect(() => {
+        const initClient = () => {
+            gapi.client.init({
+                clientId: clientId,
+                scope: ''
+            });
+        };
+        gapi.load('client:auth2', initClient);
+    });
+
+	const onSuccess = (res:any) => {
+        console.log('success', res);
+        // navigate("/redirect");
+    };
+
+    const onFailure = (err:any) => {
+        console.log('failed', err);
+    };
+
+	
 	return (
 		<div className='flex flex-1 items-center justify-center w-screen h-screen'>
+			
 			<GoogleLogin
-				clientId='788153552342-gkgingf8gsm5t1osoa7s7otiaca8kvf5.apps.googleusercontent.com'
-				buttonText='Login'
-				onSuccess={responseGoogle}
-				onFailure={responseGoogle}
-				cookiePolicy={'single_host_origin'}
-			>
-				<span> Login with Google</span>
-			</GoogleLogin>
-			{/* , document.getElementById('googleButton') */}
+                    clientId={clientId}
+                    buttonText="Sign in with Google"
+                    onSuccess={onSuccess}
+                    onFailure={onFailure}
+                    cookiePolicy={'single_host_origin'}
+                    isSignedIn={true}
+                />
+				
 		</div>
 	);
 };
